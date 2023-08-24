bf983308fbc9e9e7161c8572d6ea3ad2058ac9a7
Hawkins <2398430768@qq.com>
2023-08-24 15:12:48 +0800

    新增toREF

---
+++ 11_src_toRef/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- vue3组件目录可以没有根标签 -->
+  <!-- hello是自定义指令 -->
+  <button @click="isShowDemo=!isShowDemo">切换隐藏/显示</button>
+  <demo v-if="isShowDemo">
+  </demo>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Demo from './components/demo.vue'
+export default {
+  name: 'App',
+  components: {
+    Demo
+  },
+  setup(){
+    let isShowDemo=ref(true)
+    return {isShowDemo}
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
