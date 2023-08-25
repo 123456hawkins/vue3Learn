3f337ea7707320797564a9bb0abf19e57f0a172e
Hawkins <2398430768@qq.com>
2023-08-25 14:17:41 +0800

    新增provide和inject

---
+++ src/components/Son.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="son">
+    <span>我是孙组件</span>
+    <h3>传递的数据：{{ car.name }}-{{ car.price }}</h3>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+
+  name: 'Son',
+  setup () {
+    // 获取数据
+    let car = inject('car')
+    return { car }
+  }
+}
+</script>
+
+<style>
+.son {
+  background: blue;
+  padding: 10px;
+}
+</style>
