7312de3b5033f1471eab1a1d0cb4a5942cdf72c9
Hawkins <2398430768@qq.com>
2023-08-25 15:14:26 +0800

    新增teleport

---
+++ 18_teleport_src/components/Son.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="son">
+    <span>我是孙组件</span>
+    <Dialog></Dialog>
+  </div>
+</template>
+
+<script>
+import Dialog from './Dialog.vue'
+export default {
+
+  name: 'Son',
+  components: { Dialog },
+  setup () {
+
+  }
+}
+</script>
+
+<style>
+.son {
+  background: blue;
+  padding: 10px;
+}
+</style>
